7657e7222ec6afccb824e70dbe47289ba46ab312
Acurve <operations.acurve@gmail.com>
2026-03-23 16:07:34 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,9 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:3000/api/v1/:path*"
+        destination: isDev
+          ? "http://127.0.0.1:3000/api/v1/:path*"
+          : `${env.API_PREFIX}/:path*`
       }
     ]
   }
